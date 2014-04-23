55b91298beb70cc48b8593b0a45632ca3eac07c6
coolawesomeman <dieselpropertyholdings@gmail.com>
2014-04-23 16:16:25 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,15 @@
 # author: coolawesomeman
+
+import turtle
+
+n = 
+length = 100
+angle = 180 - (180 * (n - 2) / n)
+numbers = [ 1, 2, 3, 4, 5]
+
+
+t = turtle.Turtle()
+
+for number in numbers:
+ t.forward(length)
+ t.left(angle)
