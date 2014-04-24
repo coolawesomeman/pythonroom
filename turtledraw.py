c9563b6a009ed555069cc1f9404962bcb0355343
coolawesomeman <dieselpropertyholdings@gmail.com>
2014-04-23 17:42:29 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -5,13 +5,19 @@ import turtle
 n = 6
 length = 100
 angle = 180 - (180 * (n - 2) / n)
-numbers = range( 5,100 )
-colors = ["red", "blue" ,"green" ,"yellow" ,"pink", "magenta"]
+numbers = range( 1,100 )
+colors = ["red", "blue" ,"green" ,"yellow" ]
+
+
+
+
 
 
 t = turtle.Turtle()
 
 for number in numbers:
- t.forward(number)
-	   
- t.left(50)
+	for color in colors:
+		t.color(color)
+		t.forward(number)
+		t.left(90)
+	t.left(8)
