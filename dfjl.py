b4f317700e1eb3cf717b5a5d385caa4e9201a0e2
coolawesomeman <dieselpropertyholdings@gmail.com>
2014-04-23 17:12:57 -0700

    pythonroom update

--- dfjl.py
+++ dfjl.py
@@ -1,1 +1,18 @@
 # author: coolawesomeman
+
+import turtle
+
+number
+
+t = turtle.Turtle()
+
+for number in numbers:
+
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
