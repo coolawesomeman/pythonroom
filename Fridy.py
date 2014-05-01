eb2c81a82ba495c3645dc53b5df1b8bd2e146521
coolawesomeman <dieselpropertyholdings@gmail.com>
2014-04-30 18:03:30 -0700

    pythonroom update

--- Fridy.py
+++ Fridy.py
@@ -5,17 +5,16 @@ total = 0
 
 def daysinMonth():
 	return 30
-
 while year < 2015:
 	total = total +1
-	if day == 30
-	day = 1
-	if month == 12
-	    month = 1
-		year = year + 1
-	else:
-		month = month + 1
-	else:
+    if day == 30:
+	    day = 1
+		if month == 12:
+	        month = 1
+		    year = year + 1
+	   else:
+		   month = month + 1
+else:
 	day = day + 1
 	
 print str(total) + "days."
